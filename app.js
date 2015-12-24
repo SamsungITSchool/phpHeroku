3fc79cd191fed21d8016f2c5634d6aeec9b56794
r.kapishev <r.kapishev@samsung.com>
2015-12-24 13:27:31 +0300

    Dashboard

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
  */
 
 var application = angular.module('FingerprintAuth', ['ngRoute']);
-application.controller('userController', function ($scope, $http, $timeout) {
+application.controller('userController', function ($scope, $http, $timeout, $window) {
     $scope.user = {};
     $scope.isShowAlert = false;
     $scope.message = '';
     $scope.login = function (user) {
         console.log('Eyy');
-        $http.post('/backend/index.php?action=login', user)
+        $http.post('backend/index.php?action=login', user)
             .then(function (response) {
                 if (response.data.error !== 0) {
                     $scope.isShowAlert = true;
@@ -17,6 +17,8 @@ application.controller('userController', function ($scope, $http, $timeout) {
                     $timeout(function () {
                         $scope.isShowAlert = false;
                     }, 3000);
+                } else {
+                    $window.location.href = 'dashboard';
                 }
                 console.log(response);
             }, function (response) {
@@ -53,6 +55,10 @@ application.config(['$routeProvider', function ($routeProvider) {
         })
         .when('/signup', {
             templateUrl: 'views/signup.view.html',
+            controller: 'userController'
+        })
+        .when('/dashboard', {
+            templateUrl: 'views/dashboard.view.html',
             controller: 'userController'
         })
         .otherwise({
